a6839950749ab6b9cd0faf0af724cf8714335a94
강정은 <je@gangjeong-eun-ui-MacBookPro.local>
2021-02-13 22:01:30 +0900

    initial version

---
+++ src/Loader.js
@@ -0,0 +1,18 @@
+export default class Loader {
+  $loader = null;
+  $loaderContent = null;
+
+  constructor() {
+    this.$loader = document.querySelector(".loader");
+    this.$loaderContent = document.querySelector(".loader-content");
+  }
+
+  show = (msg) => {
+    this.$loaderContent.innerText = msg;
+    this.$loader.style.display = "";
+  }
+
+  hide = () => {
+    this.$loader.style.display = "none";
+  }
+}
